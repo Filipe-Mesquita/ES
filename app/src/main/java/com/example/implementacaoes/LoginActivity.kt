12d4aeec77eeffc0b407128afd88b7521125f4bf
Filipe-Mesquita <filipeslb2004@gmail.com>
2024-12-19 17:20:25 +0000

    login Firebase

--- app/src/main/java/com/example/implementacaoes/LoginActivity.kt
+++ app/src/main/java/com/example/implementacaoes/LoginActivity.kt
@@ -1,14 +1,24 @@
 package com.example.implementacaoes
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,7 +29,13 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
+        auth = Firebase.auth
+
+
         val registBTN = findViewById<AppCompatButton>(R.id.registarBtn)
+        val loginBTN = findViewById<AppCompatButton>(R.id.loginBtn)
+        val emailInput = findViewById<EditText>(R.id.emailEditView)
+        val passwordInput = findViewById<EditText>(R.id.passwordEditView)
 
         registBTN.setOnClickListener()
         {
@@ -27,5 +43,43 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        loginBTN.setOnClickListener()
+        {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+
+                        Toast.makeText(
+                            baseContext,
+                            "Login efetuado com sucesso!",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                        val intent = Intent(this@LoginActivity, StockActivity::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Dados de login incorretos!",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        updateUI(null)
+                    }
+                }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
     }
 }
